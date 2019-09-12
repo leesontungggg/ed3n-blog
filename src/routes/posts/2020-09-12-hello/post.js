fb473e4930e4fbe3795da0c7d5945b7fd553592d
Eden Le <nhockbake1@gmail.com>
2019-09-13 01:18:34 +0700

    Add 12/9 post

---
+++ src/routes/posts/2020-09-12-hello/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Some new idea today!`,
+  tags: ['studio', 'career','music'],
+  spoiler: "Today I got some interesting ideas.",
+  getContent: () => import('./document.mdx'),
+}
