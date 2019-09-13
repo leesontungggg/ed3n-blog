6e0c9ee364057e00aa6a5ef27ae479999072bd36
huylhDSV <huylh@dgroup.co>
2019-09-13 07:33:20 +0700

    -Add post 13/9

---
+++ src/routes/posts/2019-09-13-morning/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Fx Tour - Can Tho`,
+  tags: ['CHH', 'fxtour','music'],
+  spoiler: "Traveling to Can Tho for Fx Tour.",
+  getContent: () => import('./document.mdx'),
+}
