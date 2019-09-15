a3312c8912370c97ae79a6461acf654c59cfffec
Eden Le <nhockbake1@gmail.com>
2019-09-15 15:48:31 +0700

    Add CanTho Recap Post

---
+++ src/routes/posts/2019-09-15-afternoon/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Fx Tour Cần Thơ Recap`,
+  tags: ['CHH', 'fxtour','music'],
+  spoiler: "Recap the feeling for Fx Tour in Cần Thơ",
+  getContent: () => import('./document.mdx'),
+}
