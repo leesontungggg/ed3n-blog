90413ac475821f1be574410049cfa643c1294b9b
Eden Le <nhockbake1@gmail.com>
2019-09-17 22:34:12 +0700

    -Add post night 17/9

---
+++ src/routes/posts/2020-09-17-night/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Best use of 22.5 December Studio`,
+  tags: ['studio', 'creative','music'],
+  spoiler: "I thought about some more ideas to best use the 22.5 December Studio",
+  getContent: () => import('./document.mdx'),
+}
