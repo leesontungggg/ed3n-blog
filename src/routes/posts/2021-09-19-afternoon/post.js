b1d04aecee47c4e1541ef9900028c8a0a654301c
Eden Le <nhockbake1@gmail.com>
2019-09-19 16:48:27 +0700

    fast push all

---
+++ src/routes/posts/2021-09-19-afternoon/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Logo for Studio 225 December`,
+  tags: ['studio', 'creative','music'],
+  spoiler: "This is a fast typo of studio 225 December",
+  getContent: () => import('./document.mdx'),
+}
