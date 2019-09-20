dfa277aaae98f965bef2a5523beb276b78cf6a1f
Tung Son Le <tungls@dgroup.co>
2019-09-20 16:05:04 +0700

    fast push all

---
+++ src/routes/posts/2021-09-20-afternoon/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Did I overthink ?`,
+  tags: ['studio', 'creative','music','225december'],
+  spoiler: "Recently, I have thought about how 225 December will look like. Maybe I overthink too much.",
+  getContent: () => import('./document.mdx'),
+}
