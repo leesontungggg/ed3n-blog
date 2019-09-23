039e8261140dd9cd4c4d74e03d20e01e4f34e372
Eden Le <nhockbake1@gmail.com>
2019-09-24 00:01:13 +0700

    fast push all

---
+++ src/routes/posts/2019-09-23-night/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Main Ideas after FxTour in Danang`,
+  tags: ['fxtour', 'danang','music','chh','app'],
+  spoiler: "Some mainly ideas after FxTour in Danang, need to focus on this",
+  getContent: () => import('./document.mdx'),
+}
