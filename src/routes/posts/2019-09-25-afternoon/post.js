455bf2d6116611de7d70ed196d4cb52537bf38fc
Eden Le <nhockbake1@gmail.com>
2019-09-25 15:33:41 +0700

    fast push all

---
+++ src/routes/posts/2019-09-25-afternoon/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Feeling after quit the current job`,
+  tags: ['life', 'career','freedom'],
+  spoiler: "Today I came to office to return all the equipments & quit the job.",
+  getContent: () => import('./document.mdx'),
+}
