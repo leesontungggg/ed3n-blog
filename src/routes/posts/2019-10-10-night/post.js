e440ec521fbc3a307bc3d024972136bf893e8f6d
Eden Le <nhockbake1@gmail.com>
2019-10-10 21:48:45 +0700

    fast push all

---
+++ src/routes/posts/2019-10-10-night/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `First Picture at 225December`,
+  tags: ['22.5december', 'studio'],
+  spoiler: "It's been a long time since my last blog. Today I took my first picture at 22.5 December studio.",
+  getContent: () => import('./document.mdx'),
+}
