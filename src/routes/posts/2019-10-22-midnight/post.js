f3bbf780729230f4a1bf48f6906d4ea770c61eca
Eden Le <nhockbake1@gmail.com>
2019-10-23 01:03:05 +0700

    fast push all

---
+++ src/routes/posts/2019-10-22-midnight/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `I don't know what to do next.`,
+  tags: ['22', 'life','lost'],
+  spoiler: "I feeling lost. I don't know what to do next.",
+  getContent: () => import('./document.mdx'),
+}
