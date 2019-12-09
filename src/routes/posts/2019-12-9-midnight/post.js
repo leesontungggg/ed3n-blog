653a3017916e9d618867bcbcb5b88c27758a569d
Eden Le <nhockbake1@gmail.com>
2019-12-10 00:17:08 +0700

    fast push all

---
+++ src/routes/posts/2019-12-9-midnight/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Avicii Tribute Tracks`,
+  tags: ['Avicii', 'music','lost'],
+  spoiler: "I had reuploaded all tracks from Avicii Tribute Concert to my soundcloud.",
+  getContent: () => import('./document.mdx'),
+}
