077013558586ee87d82225c59055767476f62ed1
Eden Le <nhockbake1@gmail.com>
2019-12-27 13:44:16 +0700

    fast push all

---
+++ src/routes/posts/2019-12-27-day/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Freedom is paramount.`,
+  tags: ['lifestyle', 'free','peace'],
+  spoiler: "Even if you’re broke, clueless, and don’t know where to start",
+  getContent: () => import('./document.mdx'),
+}
