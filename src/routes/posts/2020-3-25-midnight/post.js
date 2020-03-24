82645882d6dfac429ab20600edfaf6b5844c48b2
Eden Lee <edenlee@Edens-iMac-Pro.local>
2020-03-24 12:31:28 -0700

    add post

---
+++ src/routes/posts/2020-3-25-midnight/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `My new hackintosh - Ryzen 5 3600`,
+  tags: ['macos', 'freelance'],
+  spoiler: "I successfully set up my new hackintosh machine.",
+  getContent: () => import('./document.mdx'),
+}
