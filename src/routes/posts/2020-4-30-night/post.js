f47a9e47dc354b969d6f4492f0c1c1933a30e951
Eden Lee <leesontungggg@gmail.com>
2020-04-30 19:11:27 +0700

    change des

--- src/routes/posts/2020-4-30-night/post.js
+++ src/routes/posts/2020-4-30-night/post.js
@@ -1,6 +1,6 @@
 export default {
   title: `30/4/2020`,
   tags: ['letter'],
-  spoiler: "Many of us spend all our lives searching for the perfect partner. But it can be really easy to know how to find your soulmate if you understand these tips.",
+  spoiler: "Many of us spend all our lives searching for the perfect partner. Me too.",
   getContent: () => import('./document.mdx'),
 }
