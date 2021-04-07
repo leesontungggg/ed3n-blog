4e2b778a6f69910c4deb548ded80e2bc75c3156b
Eden Le <edenlee@Edens-MacBook-Pro.local>
2021-04-07 15:38:37 +0700

    add new post

---
+++ src/routes/posts/2021-4-07-day/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Digging into Terminal working space`,
+  tags: ['terminal','devlife'],
+  spoiler: "I am trying to use Terminal Neovim as my normal IDE instead of VS Code",
+  getContent: () => import('./document.mdx'),
+}
